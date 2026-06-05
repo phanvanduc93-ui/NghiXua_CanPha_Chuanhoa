c6bbbf56cf3cadd426688451fd829f6c3b761e00
phanvanduc93-ui <phanvanduc93@gmail.com>
2026-06-05 19:58:12 +0700

    can

---
+++ app.js
@@ -0,0 +1,11 @@
+function hienThiCot() {
+
+    document.getElementById("ketqua").innerHTML = `
+        <h2>🗼 Cột 01</h2>
+
+        <p>📦 Hòm 01 (4 công tơ)</p>
+
+        <p>📦 Hòm 02 (4 công tơ)</p>
+    `;
+
+}
